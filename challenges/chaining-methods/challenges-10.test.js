7631c64aa6d007f789448da7f2f0dc52d2485327
Marie Antons <maantons@mail.fhsu.edu>
2020-04-11 21:47:34 -0500

    completed challenge 4-5

--- challenges/chaining-methods/challenges-10.test.js
+++ challenges/chaining-methods/challenges-10.test.js
@@ -124,7 +124,7 @@ let starWarsData = [{
 }];
 
 let findMaleAndFemale = (data) => {
-    
+  return data.filter((character) => (character.gender === 'male' || character.gender === 'female')).map(character => character.name).join(' and ');    
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -134,7 +134,13 @@ Write a function named findShortest that, given the Star Wars data from Challeng
 ------------------------------------------------------------------------------------------------ */
 
 let findShortest = (data) => {
-  
+  const shortestHeight = data.reduce((current, character) => {
+    if(parseInt(character.height) < current) {
+      current = character.height;
+    }
+    return current;
+  }, 1000);
+  return data.filter(character => character.height === shortestHeight)[0].name;
 };
 
 /* ------------------------------------------------------------------------------------------------
